cf33ab2d7601f04a4dae4db2cf0f9bb1de84ffc3
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-02 17:29:36 -0300

    title on show page

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,5 +1,5 @@
 module CardsHelper
   def text_label(card)
-    "#{t('date.month_names')[card.month_idx]} - #{Card.human_attribute_name('class_idx')} #{card.class_idx} - #{card.question_idx}"
+    "#{t('date.month_names')[card.month_idx]} - #{Card.human_attribute_name('class_idx')} #{card.class_idx} - #{Card.human_attribute_name('question_idx')} #{card.question_idx}"
   end
 end
