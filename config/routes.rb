bdc9d796c8559657a28832a67da8319b0d16b8c6
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-02 19:18:17 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :test
 
   map.resources :cards
-  map.root :controller => :tests
+  map.root :controller => 'tests'
 
   map.connect ':lang/:controller/:action/:id'
   map.connect ':lang/:controller/:action/:id.:format'
