1de53c633ba6e89a1ccc4a2d9f97d5831a07b02f
Luis Perichon <luigi@luigi-laptop.(none)>
2010-01-03 01:24:46 -0300

    fix strange error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
-  # LP:strange fix... we have to duplicate this helper method call, even though it is in authentication.rb
-  helper_method :logged_in?
+  filter_parameter_logging :password
+  
+  helper_method :current_user, :logged_in?, :redirect_to_target_or_default
 
   # start over if test abandoned for more than 1 hour, or if there are no more incorrect or ananwered questions
   def start_over?
@@ -17,5 +16,38 @@ class ApplicationController < ActionController::Base
     ping = DateTime.parse(cookies[:ping]) if cookies[:ping]
 
     return ping.nil? || ping < 1.hour.ago
+  end
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.record
+  end
+
+  def logged_in?
+    current_user
+  end
+
+  def login_required
+    unless logged_in?
+      flash[:error] = "You must first log in or sign up before accessing this page."
+      store_target_location
+      redirect_to login_url
+    end
+  end
+
+  def redirect_to_target_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  private
+
+  def store_target_location
+    session[:return_to] = request.request_uri
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'authlogic'
+  config.gem 'authlogic', :version => '2.1.1'
   config.gem 'formtastic'
   config.gem 'inherited_resources'
 
